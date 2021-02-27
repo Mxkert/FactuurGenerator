af5b6be5e4271c85befc878b164965da4420a944
Mxkert <mikehendriks94@gmail.com>
2021-02-27 14:56:33 +0100

    T

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ app.use('/api/invoices', invoices);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
+  app.get("/serviceWorker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "src", "serviceWorker.js"));
+  });
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
